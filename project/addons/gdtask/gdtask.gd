1f85cb73fc8654690a939cdcd23a134267d5db73
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-06-14 21:16:02 +0930

    re-worked testing scene
    renamed CANCELED to CANCELLED
    added started and completed signals
    added some debug prints while developing
    re-worked run() to be more robust and user friendly
    removed wait()
    allowed cancel() at any time
    removed timeout() function
    added Timeout class override
    added static CancelAfter factory func for timeout class
    re-worked Repeater class
    change the way subclasses are instanced in the factory functions and broke a few things

--- project/addons/gdtask/gdtask.gd
+++ project/addons/gdtask/gdtask.gd
@@ -11,11 +11,13 @@ enum Status {
 	PENDING,	# The default state, waiting to be run
 	INPROGRESS,	# Currently running
 	COMPLETED,	# finished running
-	CANCELED	# finished running by cancellation
+	CANCELLED	# finished running by cancellation
 }
 #endregion
 
 #region Signals
+signal started
+signal completed( product )
 signal finished( status : Status )
 #endregion
 
@@ -32,6 +34,13 @@ var next : GDTask
 var previous : GDTask
 #endregion
 
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_POSTINITIALIZE:
+			print("gdtask(%s) - post-initialise" %id )
+		NOTIFICATION_PREDELETE:
+			print("gdtask(%s) - pre-delete" %id )
+
 #region Basic Methods
 ## by default the constructor does not action the callable, it simply creates the object.
 func _init( _callable: Callable, _bindings : Array = [] ):
@@ -39,6 +48,12 @@ func _init( _callable: Callable, _bindings : Array = [] ):
 	next_id += 1
 	callable = _callable
 	bindings = _bindings
+	
+	#DEBUG PRINT
+	if callable.is_custom():
+		print("gdtask(%s) initialised | %s( %s ) " % [id,"<anonymous lambda>",bindings] )
+	else:
+		print("gdtask(%s) initialised | %s( %s ) " % [id,callable.get_method(),bindings] )
 
 
 ## Reset clears the product and sets the status to pending, as if it was just created.
@@ -48,45 +63,56 @@ func reset():
 
 
 ## Run the task, pending the completion of previous tasks
-# wont run if the task is not in the pending state.
-# Interestingly, if there is a next task, and no bindings associated with that task,
-# then the product of the this task will be bound to it.
-# TODO it might be better to attach the product at the end of the bindings.
-# we can run the task synchronously by calling it with the await keyword.
-# ie await run()
-# else it is run as a coroutine in the background.
+# run() is asynchronous
+# await run() is synchronous
+# repeated run() calls will just trigger a finished signal emission, or await the finished signal
 func run() -> void:
-	if status != Status.PENDING: return
-	if previous && previous.status == Status.PENDING: await previous.run()
-	if status == Status.PENDING:
-		status = Status.INPROGRESS
+	match status:
+		Status.PENDING:
+			status = Status.INPROGRESS
+			started.emit()
+		Status.INPROGRESS:
+			await finished
+			return
+		_:
+			finished.emit( status )
+			return
 		
-		#var object = callable.get_object()
-		#if object is Node: object = object.name
-		#var method_name = "%s" % callable.get_method() if callable.is_standard() else "lambda"
-		#var _indent = Util.printy( "run()", [], self )
-		#Util.printy( "\t%s.%s( %s ) )", [object, method_name , bindings] )
-		
-		product = await callable.callv( bindings )
-		if status != Status.CANCELED: status = Status.COMPLETED
-		finished.emit( status )
-	if status == Status.CANCELED: return
-	if next && next.status == Status.PENDING:
+	if previous && previous.status == Status.PENDING:
+		await previous.run()
+		if previous.status == Status.CANCELLED:
+			status = Status.CANCELLED
+			finished.emit( status )
+
+	#DEBUG PRINT
+	#var object = callable.get_object()
+	#if object is Node: object = object.name
+	#var method_name = "%s" % callable.get_method() if callable.is_standard() else "lambda"
+	#var _indent = Util.printy( "run()", [], self )
+	#Util.printy( "\t%s.%s( %s ) )", [object, method_name , bindings] )
+	
+	# Run our task
+	product = await callable.callv( bindings )
+	
+	if status != Status.CANCELLED: status = Status.COMPLETED
+	
+	finished.emit( status )
+	completed.emit( product )
+	if status == Status.CANCELLED: return
+	
+	# if there is a next task, run it
+	if next:
+		# TODO it might be better to attach the product at the end of the bindings.
+		# pass the product of this task to the next task
 		if product && not next.bindings: next.bindings = [product]
 		next.run()
 
-## wait for any previous tasks in the chain to complete, before awaiting the finished signal
-# This function does not trigger the gdtask to run.
-func wait():
-	if previous: await previous.wait()
-	await finished
-
 
 ## Cancel the task, and propogate forward and backwards through the chain
-# This only cancels tasks that have not started yet.
+# This cannot stop an existing function call from taking place.
+# But it can prevent further run calls to next and previous tasks
 func cancel():
-	if status != Status.PENDING: return
-	status = Status.CANCELED
+	status = Status.CANCELLED
 	finished.emit( status )
 	# Propagate cancellation
 	if previous && previous.status == Status.PENDING: previous.cancel()
@@ -98,18 +124,10 @@ func result():
 	match status:
 		Status.PENDING: await run()
 		Status.INPROGRESS: await finished
-		Status.CANCELED: return null
+		Status.CANCELLED: return null
 		Status.COMPLETED: pass
 	return product
-
-
-## Attach a timeout to the task that cancels it
-func timeout( seconds : float ):
-	var scene_tree = Engine.get_main_loop() as SceneTree
-	if not scene_tree: print("Unable to get the SceneTree from Engine")
-	else: scene_tree.create_timer(seconds).timeout.connect( func(): cancel() )
-	return self
-
+	
 
 ## then attaches a new gdtask to perform after this one completes and returns the new task.
 func then( _callable : Callable, _bindings : Array = [] ) -> GDTask:
@@ -126,47 +144,94 @@ class Watcher extends GDTask:
 	var scene_tree : SceneTree
 
 	func watcher():
-		if callable.callv(bindings):
+		if callable.callv( bindings ):
 			status = Status.COMPLETED
 			finished.emit( status )
 			scene_tree.process_frame.disconnect( watcher )
 			if next: next.run()
+	
+	func _on_started():
+		if scene_tree: scene_tree.process_frame.connect( watcher )
+		else: cancel()
 
 	func _init( _callable: Callable, _bindings : Array ):
 		super._init( _callable, _bindings )
 		scene_tree = Engine.get_main_loop() as SceneTree
-		if scene_tree: scene_tree.process_frame.connect( watcher )
-		else: printerr("Unable to get the SceneTree from Engine")
+		if not scene_tree: printerr("Unable to get the SceneTree from Engine")
 
 
 class NextFrame extends GDTask:
 	var scene_tree : SceneTree
-
+	var start_frame : int
+	
+	func cleanup( _status ):
+		scene_tree.process_frame.disconnect( run )
+	
+	func run():
+		if scene_tree.get_frame() >= start_frame+1: super.run()
+		
 	func _init( _callable : Callable, _bindings : Array = [] ):
 		super._init( _callable, _bindings )
 		scene_tree = Engine.get_main_loop() as SceneTree
-		if not scene_tree: printerr("Unable to get the SceneTree from Engine")
-		else:
-			scene_tree.process_frame.connect( run )
-			finished.connect(func(_product): scene_tree.process_frame.disconnect( run ) )
+		if not scene_tree: printerr("Unable to get the SceneTree from Engine"); return
+		start_frame = scene_tree.get_frame()
+		scene_tree.process_frame.connect( run )
+		finished.connect( cleanup )
 
 class Repeater extends GDTask:
 	var scene_tree : SceneTree
 	var seconds : float
 	var ntimes : int = -1
+	
+	func run() -> void:
+		match status:
+			Status.PENDING:		status = Status.INPROGRESS
+			Status.CANCELLED:	finished.emit( status )
+			Status.COMPLETED:	finished.emit( status )
+			Status.INPROGRESS:	pass
+			
+		if previous && previous.status == Status.PENDING:
+			await previous.run()
+			if previous.status == Status.CANCELLED:
+				status = Status.CANCELLED
+				finished.emit( status )
 
-	func repeat():
-		if ntimes > 0: ntimes -= 1
-		if not ntimes: status = Status.COMPLETED; return
-		await run()
-		scene_tree.create_timer(seconds).timeout.connect( repeat )
+		#DEBUG PRINT
+		#var object = callable.get_object()
+		#if object is Node: object = object.name
+		#var method_name = "%s" % callable.get_method() if callable.is_standard() else "lambda"
+		#var _indent = Util.printy( "run()", [], self )
+		#Util.printy( "\t%s.%s( %s ) )", [object, method_name , bindings] )
+		
+		# Run our task
+		if ntimes:
+			product = await callable.callv( bindings )
+			completed.emit( product )
+			ntimes -= 1
+		
+		if ntimes:
+			scene_tree.create_timer(seconds).timeout.connect( run )
+		elif status == Status.CANCELLED:
+			finished.emit( status )
+			return
+		else:
+			status = Status.COMPLETED;
+			finished.emit( status )
+		
+		# if there is a next task, run it
+		if status != Status.CANCELLED and next:
+			# pass the product of this task to the next task
+			if product && not next.bindings: next.bindings = [product]
+			next.run()
+		
 
-	func _init( _seconds: float, _callable : Callable, _bindings : Array = [] ):
+	func _init( _ntimes : int, _seconds: float, _callable : Callable, _bindings : Array = [] ):
 		super._init( _callable, _bindings )
 		seconds = _seconds
+		ntimes = _ntimes
 		scene_tree = Engine.get_main_loop() as SceneTree
 		if not scene_tree: printerr("Unable to get the SceneTree from Engine"); return
-		repeat()
+
 
 class Delay extends GDTask:
 	var scene_tree : SceneTree
@@ -179,24 +244,57 @@ class Delay extends GDTask:
 		if not scene_tree: printerr("Unable to get the SceneTree from Engine"); return
 		scene_tree.create_timer(seconds).timeout.connect( run )
 
+#TODO Timeout is in progress
+class Timeout extends GDTask:
+	var scene_tree : SceneTree
+	var seconds : float
+	var timeout : SceneTreeTimer
+	
+	func _on_started():
+		timeout = scene_tree.create_timer( seconds )
+		timeout.timeout.connect( cancel )
+	
+	func _on_finished( _status : Status ):
+		timeout.timeout.disconnect( cancel )
+	
+	func _init( _seconds : float, _callable : Callable, _bindings : Array = [] ):
+		super._init( _callable, _bindings )
+		seconds = _seconds
+		scene_tree = Engine.get_main_loop() as SceneTree
+		if not scene_tree: printerr("Unable to get the SceneTree from Engine"); return
+		started.connect( _on_started )
+		finished.connect( _on_finished )
 #endregion
 
 #region Factory functions for specialisations
 
 ## WaitUntil
 static func WaitUntil( _callable : Callable, _bindings : Array = [] ) -> GDTask:
-	return Watcher.new( _callable, _bindings )
+	var task = Watcher.new( _callable, _bindings )
+	task.run()
+	return task
 
 ## WaitFrame
 static func WaitFrame( _callable : Callable, _bindings = [] ) -> GDTask:
-	return NextFrame.new( _callable, _bindings )
+	var task = NextFrame.new( _callable, _bindings )
+	task.run()
+	return task
 
 ## RepeatEvery
-static func RepeatEvery( seconds : float, _callable : Callable, _bindings : Array = [] ):
-	return Repeater.new( seconds, _callable, _bindings )
+static func RepeatEvery( seconds : float, ntimes : int, _callable : Callable, _bindings : Array = [] ):
+	var task = Repeater.new( ntimes, seconds, _callable, _bindings )
+	task.run()
+	return task
 
 ## DelayFor
 static func DelayFor( seconds : float, _callable : Callable = func(): return, _bindings : Array = [] ):
-	return Delay.new( seconds, _callable, _bindings )
+	var task = Delay.new( seconds, _callable, _bindings )
+	task.run()
+	return task
+
+static func CancelAfter( seconds : float, _callable : Callable = func(): return, _bindings : Array = [] ):
+	var task = Timeout.new( seconds, _callable, _bindings )
+	task.run()
+	return task
 
 #endregion
--- project/gdtask_tests.gd
+++ project/gdtask_tests.gd
@@ -13,41 +13,93 @@ extends RichTextLabel
 	#	- WaitChange - watch a variable for changes, might be the same as WaitUntil
 	#	- NetworkRequest	- keep track of rpc calls that have results
 
-enum Values {
-	NONE,
-	SHORT,
-	LONG
-}
+var last_value : float
 
-@onready var value = Values.NONE
-func print_value( wanted ) -> void:
-	var _wanted = Values.keys()[wanted]
-	var _value = Values.keys()[value]
-	if wanted == value: print( "wanted: %s, got: %s" % [_wanted, _value] )
-	else: printerr( "wanted: %s, got: %s" % [_wanted, _value] )
+func generic( duration : float = 0, value : float = 0 ) -> float:
+	#print_rich("[color=slate_grey]generic( duration: %s, value: %s ) -> %s[/color]" % [duration, value, duration + value] )
+	if duration: await get_tree().create_timer( duration ).timeout
+	last_value = value
+	return duration + value
 
-func short_func() -> void:
-	print( "short func")
-	value = Values.SHORT
+static var repeat_count = 0
+
+func repeat_func() -> void:
+	repeat_count += 1
+	print( "repeater: %s" % repeat_count )
 	
-func long_func() -> void:
-	await get_tree().create_timer(1).timeout
-	print( "long_func")
-	value = Values.LONG
+func check( test : String, have, want ):
+	var txt = "[b]%s - have: %s, want: %s[/b]" % [test, have, want]
+	if have == want:
+		print_rich( txt, " - [color=green]OK[/color]" )
+	else:
+		printerr(txt)
 
 func _ready():
-	var short_task = GDTask.new( short_func )
-	var long_task = GDTask.new( long_func )
-	if value != Values.NONE:
-		printerr("Initial conditions not met")
-		return
+	var long_task = GDTask.new( generic, [1, 2] )
 	
+	last_value = 999
+	
+	# running a task is a non blocking operation
 	long_task.run()
-	print_value(Values.NONE)
-	short_task.run()
-	print_value(Values.SHORT)
-	await long_task.result()
-	print_value(Values.LONG)
+	check( "task.run() is non blocking", last_value, 999 )
+	
+	# runnin an inprogress task is also non blocking, but has little effect.
+	# the function is made, and awaits the finished result, but returns instantly.
+	long_task.run()
+	
+	# calling await on an in-progress task will await the result
+	await long_task.run()
+	check("await task.run() on task in progress", last_value, 2 )
+	
+	# awaiting the result of a completed task will return the product
+	check( "await task.result() on completed task is OK", await long_task.result(), 3 )
+	
+	# reset product and status, ie return to baseline
+	long_task.reset()
+	
+	# awaiting the result of a fresh task will run it
+	check( "await task.result() runs task to get result", await long_task.result(), 3 )
+	
+	long_task.reset()
+	long_task.run()
+	
+	# Cancelling a task cant stop a callable that is already in progress,
+	# but it can propagate the call to previous and subsequent tasks, or tasks that repeat.
+	long_task.cancel()
+	check( "task.cancel on task in progress", long_task.status, GDTask.Status.CANCELLED )
+
+	
+	## GDTask.CancelAfter
+	# creating a task doesnt run it.
+	var timeout_task : GDTask = GDTask.CancelAfter( 1, generic, [2, 3])
+	check( "status before task.run() on timeout", timeout_task.status, GDTask.Status.PENDING )
+	timeout_task.run()
+	check( "status after task.run() on timeout", timeout_task.status, GDTask.Status.INPROGRESS )
+	await timeout_task.run()
+	check( "status after await task.run() on timeout", timeout_task.status, GDTask.Status.CANCELLED )
+	
+	
+	## GDTask.RepeaterEvery
+	await GDTask.RepeatEvery( 0.1, 5, repeat_func ).finished
+	check( "Repeat count is correct", repeat_count,  5 )
+	
+	# next frame
+	var current_frame = get_tree().get_frame()
+	print("frame: ", current_frame )
+	var frame = await GDTask.WaitFrame( func(): return get_tree().get_frame() ).result()
+	print( "frame: ", frame )
+	
+	## GDTask.DelayFor()
+	## GDTask.WaitFrame()
+	## GDTask.WaitUntil()
+	
+	GDTask.DelayFor(1, func():
+		print_rich("[color=purple]quitting[/color]")
+		get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+		get_tree().quit()
+		)
+	
+	
 	
 	
 	
